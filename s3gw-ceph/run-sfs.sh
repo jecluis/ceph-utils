87bdb06da89869bc2e6eec7945b01a16d14768c9
Joao Eduardo Luis <joao@suse.com>
2023-10-20 18:57:38 +0100

    ceph: make run-sfs.sh better
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- s3gw-ceph/run-sfs.sh
+++ s3gw-ceph/run-sfs.sh
@@ -1,27 +1,87 @@
 #!/bin/bash
 
-DEBUG=${DEBUG:-10}
-WITH_GDB=${WITH_GDB:-""}
+ccache_dir=${S3GW_CCACHE_DIR:-}
+nproc=${S3GW_NPROC:-20}
+debug=${S3GW_DEBUG:-10}
+with_gdb=${S3GW_WITH_GDB:-0}
+no_build=${S3GW_NO_BUILD:-0}
+build_only=${S3GW_BUILD_ONLY:-0}
 
 d=${PWD##*/}
 backend=${BACKEND:-"sfs"}
+
+usage() {
+  cat <<EOF
+usage: $0 [options]
+
+options:
+  -b | --build-only     Only build, don't run.
+  -n | --no-build       Don't build, only run.
+  -c | --ccache DIR     Specify ccache directory.
+  -p | --proc VALUE     Use VALUE number of processes.
+  -d | --debug VALUE    Set rgw debug to VALUE.
+  -g | --with-gdb       Run with gdb.
+  -h | --help           Show this message.
+
+env:
+  ccache dir:  ${ccache_dir}
+       nproc:  ${nproc}
+       debug:  ${debug}
+EOF
+}
+
+while [[ $# -gt 0 ]]; do
+
+  case $1 in
+    -b|--build-only) build_only=1 ;;
+    -c|--ccache)
+      ccache_dir=$2
+      shift 1
+      ;;
+    -p|--proc)
+      nproc=$2
+      shift 1
+      ;;
+    -d|--debug)
+      debug=$2
+      shift 1
+      ;;
+    -g|--with-gdb)
+      with_gdb=1
+      ;;
+    -n|--no-build)
+      no_build=1
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "unknown option '$1'" >/dev/stderr
+      exit 1
+      ;;
+  esac
+
+  shift 1
+
+done
+
+[[ -z "${ccache_dir}" ]] && \
+  echo "missing ccache dir" >/dev/stderr && exit 1
+
+[[ ! -d "${ccache_dir}" ]] && \
+  echo "ccache dir at '${ccache_dir}' does not exist" >/dev/stderr && exit 1
 
 if [[ "${d}" != "build" ]]; then
 	[[ ! -d "build" ]] && echo "can't find build directory" && exit 1
 	pushd build || exit 1
 fi
 
-build_only=false
 
-if [[ $# -gt 0 ]]; then
-  if [[ "${1}" == "-b" ]]; then
-    build_only=true
-  fi
-fi
-
-
-CCACHE_DIR=/srv/containers/joao/s3gw-ceph-ccache ninja -j 20 bin/radosgw || \
+if [[ $no_build -eq 0 ]]; then
+  CCACHE_DIR=${ccache_dir} ninja -j ${nproc} bin/radosgw || \
 	exit 1
+fi
 
 args="--rgw-sfs-data-path $(pwd)/dev/rgw.foo"
 if [[ "${backend}" == "dbstore" ]]; then
@@ -29,7 +89,7 @@ if [[ "${backend}" == "dbstore" ]]; then
 fi
 
 maybe_gdb() {
-	if [[ -n "${WITH_GDB}" ]]; then
+	if [[ -n "${with_gdb}" ]]; then
 		gdb --args $*
 	else
 		$*
@@ -39,7 +99,7 @@ maybe_gdb() {
 
 [[ ! -d "$(pwd)/dev/rgw.foo" ]] && mkdir -p $(pwd)/dev/rgw.foo
 
-if $build_only ; then
+if [[ $build_only -eq 1 ]]; then
   echo "not running, build-only requested!"
   exit 0
 fi
@@ -48,7 +108,7 @@ maybe_gdb bin/radosgw \
 	--rgw-backend-store=${backend} \
 	${args} \
 	-i foo \
-	--debug-rgw ${DEBUG} \
+	--debug-rgw ${debug} \
 	--no-mon-config \
 	--rgw-data $(pwd)/dev/rgw.foo \
 	--run-dir $(pwd)/dev/rgw.foo \
